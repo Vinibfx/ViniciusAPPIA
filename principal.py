8468a1c8a17d8473ce661ac7be7bef73ea1c3b8f
Vinibfx <116393152+Vinibfx@users.noreply.github.com>
2022-11-29 22:31:17 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -53,6 +53,7 @@ f20 = st.number_input('Digite tendência do histogramao',min_value=-1,max_valeu=
 
 if st.button('Qual a classificação do seu feto?'):
   resposta = floresta.predict([[f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20]])#fara a predicao
+  st.write(resposta)
   if resposta == 1:
     st.write('Normal')
 # if resposta == 2:
