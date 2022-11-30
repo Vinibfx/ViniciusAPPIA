274dd80141541ca0b9698817f9794036cbdabe70
Vinibfx <116393152+Vinibfx@users.noreply.github.com>
2022-11-29 21:15:18 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -39,21 +39,21 @@ f7 = st.number_input('Digite Número de DPs por segundo'min_value=0, max_value=0
 f8 = st.number_input('Digite Porcentagem de tempo com variabilidade anormal de curto prazo'min_value=12, max_value=87))
 f9 = st.number_input('Digite Valor médio da variabilidade de curto prazo'min_value=0.20, max_value=7))
 f10 = st.number_input('Digite Porcentagem de tempo com variabilidade anormal de longo prazo'min_value=0, max_value=91))'
-f11 = st.number_input('Digite Valor médio da variabilidade de longo prazo'min_value=
-f12 = st.number_input(
-f13 = st.number_input(
-f14 = st.number_input(
-f15 = st.number_input(
-f16 = st.number_input(
-f17 = st.number_input(
-f18 = st.number_input(
-f19 = st.number_input(
-f20 = st.number_input(
-f21 = st.number_input(
+f11 = st.number_input('Digite Valor médio da variabilidade de longo prazo'min_value=50,max_valeu+159))
+f12 = st.number_input('Digite Valor mínimo do histograma'min_value=50,max_valeu=159))
+f13 = st.number_input('Digite Valor máximo do histograma'min_value=152,max_valeu=238))
+f14 = st.number_input('Digite Número de picos no histograma do exame'min_value=0,max_valeu=18))
+f15 = st.number_input('Digite Número de zeros no histograma do exame'min_value=0,max_valeu=10))
+f16 = st.number_input('Digite modo hist'min_value=60,max_valeu=187))
+f17 = st.number_input('Digite Hist significa'min_value=73,max_valeu=187))
+f18 = st.number_input('Digite Hist mediana'min_value=77,max_valeu=186))
+f19 = st.number_input('Digite variância histórica'min_value=0,max_valeu=269))
+f20 = st.number_input('Digite tendência do histogramao'min_value=-1,max_valeu=1))
+f21 = st.number_input('Digite Saúde fetal' 1-Normal ,2-Suspeito ,3-Patológico))
 
 if st.button('O sujeito tera AVC?'):
-  resposta = floresta.predict([[f1,f2,f3,f4,f5,f6,f7,f8,f9,f10]])#fara a predicao
+  resposta = floresta.predict([[f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21]])#fara a predicao
   if resposta == 0:
-    st.write('O sujeito nao tera AVC!')
+    st.write('Qual classificação é a do seu FETO??')
   else:
-    st.write('O sujeito tera AVC!')
+    st.write('classificação é a do seu FETO??')
