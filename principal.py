3cd0cd92779385d6754f82ba9d1c0e6ee41c9eae
Vinibfx <116393152+Vinibfx@users.noreply.github.com>
2022-11-29 22:56:25 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -55,9 +55,9 @@ if st.button('Qual a classificação do seu feto?'):
   resposta = floresta.predict([[f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20]])#fara a predicao
   
   st.write('testes:',resposta)
-  if resposta == 0:
+  if resposta == 1:
     st.write('Normal')
- if resposta == 1:
+ if resposta == 2:
     st.write('Suspeito')
  if resposta == 3:
     st.write('Patológico')
